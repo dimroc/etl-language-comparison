f0b703fdedada91dff79add8aa3274c18f8ac5a4
José Valim <jose.valim@plataformatec.com.br>
2015-05-14 20:56:12 +0200

    USe async/await pattern for simplicity

--- elixir/lib/mapper.ex
+++ elixir/lib/mapper.ex
@@ -1,21 +1,8 @@
 defmodule Mapper do
   def map(input_dir) do
-    input_files = generate_input_files(input_dir)
-
-    input_files
-    |> Enum.each(fn t -> Task.start_link(MapActor, :map, [self(), t]) end)
-
-    listen_for_children(length(input_files), [])
-  end
-
-  defp listen_for_children(0, acc) do
-    acc
-  end
-
-  defp listen_for_children(count, acc) do
-    receive do
-      {:ok, mapping} -> listen_for_children(count - 1, [mapping|acc])
-    end
+    generate_input_files(input_dir)
+    |> Enum.map(&Task.async(MapActor, :map, [&1]))
+    |> Enum.map(&Task.await(&1, :infinity))
   end
 
   defp generate_input_files(input_dir) do
