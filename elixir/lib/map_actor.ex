a34fa68feff15ffdd404f6597ee5a9abb96cf631
bbense <bbense@gmail.com>
2015-05-14 17:47:34 -0700

    Change to elixir map to reduce run time in half.
    You're parsing every tweet when there is no need to do so.

--- elixir/lib/map_actor.ex
+++ elixir/lib/map_actor.ex
@@ -1,20 +1,18 @@
 defmodule MapActor do
   @pattern ~r/knicks/i
-
+  
   def map(file) do
+    match = @pattern
     #IO.puts "mapping file #{file}"
     File.stream!(file)
-    |> Stream.map(fn line -> String.split(line, "\t") end)
+    |> Stream.filter( fn(line) -> Regex.match?(match,line) end)
     |> Enum.reduce(HashDict.new, &reduce_stream/2)
   end
 
-  def reduce_stream([_, hood, _, message], acc) do
+  def reduce_stream(message, acc) do
+    hood = String.split(message,"\t") |> Enum.at(1)
     hook = String.to_atom(hood)
-    #if String.contains? String.downcase(message), "knicks" do
-    if message =~ @pattern do
-      HashDict.update(acc, hook, 1, &(&1 + 1))
-    else
-      acc
-    end
+    HashDict.update(acc, hook, 1, &(&1 + 1))
   end
+
 end
