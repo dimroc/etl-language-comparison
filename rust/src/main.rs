4278d3c2ac983287cfd016952be2694a736190af
Andrew Bennett <potatosaladx@gmail.com>
2015-05-20 14:15:03 -0600

    Add regex and substring mode matching for rust implementation.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,10 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
+extern crate regex;
+
 use std::fs::{File};
 use std::io::prelude::*;
 use std::io::{BufReader};
@@ -8,12 +15,27 @@ use std::ascii::AsciiExt;
 use std::collections::HashMap;
 
 fn main() {
+	let args: Vec<String> = env::args().collect();
+	let program = args[0].clone();
+	let mut options = Options::new();
+	options.optflag("h", "help", "print this help menu");
+	options.optflag("s", "substring", "use substring matching");
+	let matches = options.parse(&args[1..]).unwrap();
+	if matches.opt_present("h") {
+		let brief = format!("Usage: {} [options]", program);
+		print!("{}", options.usage(&brief));
+		return;
+	}
+	let re = regex::Regex::new(r"(?i)knicks").unwrap();
+	let substring = matches.opt_present("s");
+
 	let (sender, receiver) = mpsc::channel();
 
 	let paths = std::fs::read_dir("../tmp/tweets").unwrap();
 	let mut n = 0;
 	for path in paths {
 		n += 1;
+		let re = re.clone();
 		let sender = sender.clone();
 		thread::spawn(move || {
 			let path = path.unwrap().path();
@@ -21,8 +43,13 @@ fn main() {
 			let mut map: HashMap<String, u64> = HashMap::new();
 			for line in reader.lines() {
 				let line = line.unwrap();
-				let line_lowercase = line.to_ascii_lowercase();
-				if line_lowercase.contains("knicks") {
+				let is_match = if substring {
+					let line_lowercase = line.to_ascii_lowercase();
+					line_lowercase.contains("knicks")
+				} else {
+					re.is_match(&line)
+				};
+				if is_match {
 					let hood = line.split("\t").nth(1).unwrap();
 					let counter = map.entry(hood.to_string()).or_insert(0);
 					*counter += 1;
@@ -54,7 +81,11 @@ fn main() {
 	values.sort();
 	values.reverse();
 
-	let mut file = File::create("../tmp/rust_output").unwrap();
+	let mut file = if substring {
+		File::create("../tmp/rust_substring_output").unwrap()
+	} else {
+		File::create("../tmp/rust_regex_output").unwrap()
+	};
 	for value in values.iter() {
 		let keys = inverted_map.get(value).unwrap();
 		let mut hoods: Vec<String> = Vec::new();
